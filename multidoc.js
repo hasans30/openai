6fbc5e81832e8d6982953f0bb6224f8bb5f543e4
Hasan uz zaman Sheikh <hasan@live.in>
2023-07-10 03:00:12 -0700

    query from personal knowledge base

--- multidoc.js
+++ multidoc.js
@@ -8,9 +8,8 @@ import { TextLoader } from "langchain/document_loaders/fs/text";
 import { DirectoryLoader } from "langchain/document_loaders/fs/directory";
 import "dotenv/config";
 
-// Load and process the text files
-// const fileName = "c:\\tmp\\test.txt";
-// const loader = new TextLoader(fileName);
+const queryText = process.argv.slice(2).join(" ");
+console.log(`searching for...`, queryText);
 
 const loader = new DirectoryLoader("./new_articles/", {
   ".txt": (path) => new TextLoader(path),
@@ -23,22 +22,28 @@ const text_splitter = new RecursiveCharacterTextSplitter();
 text_splitter.chunkSize = 1000;
 text_splitter.chunkOverlap = 200;
 
-//   (chunk_size = 1000),
-//   (chunk_overlap = 200)
-// );
-// const texts = text_splitter.split_documents(documents);
 const texts = await text_splitter.splitDocuments(documents);
 
 console.log("loaded text form directory (our knowledge db)", texts.length);
-
-// Embed and store the texts
-// Supplying a persist_directory will store the embeddings on disk
-const persistDirectory = "db";
 
 // here we are using OpenAI embeddings but in future we will swap out to local embeddings
 const embedding = new OpenAIEmbeddings();
 
 const vectorStore = await MemoryVectorStore.fromDocuments(documents, embedding);
 
-const resultOne = await vectorStore.similaritySearch("use support ticket", 1);
+// This is how to perform similarity search
+/*
+const resultOne = await vectorStore.similaritySearch("kalam", 1);
 console.log(resultOne);
+*/
+
+const retriever = vectorStore.asRetriever();
+
+const model = new OpenAI({
+  modelName: "text-davinci-003",
+});
+const chain = RetrievalQAChain.fromLLM(model, retriever);
+const res = await chain.call({
+  query: queryText,
+});
+console.log({ res });
