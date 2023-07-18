e69e6f02f6108b42c915b928587d43d415eb473a
Hasan uz zaman Sheikh <hasan@live.in>
2023-07-18 14:48:29 +0530

    without textloader, it was failing to load doc

--- py/openai/custom_file_QA.py
+++ py/openai/custom_file_QA.py
@@ -4,6 +4,7 @@ from langchain.vectorstores import FAISS
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain import OpenAI
 from langchain.chains import RetrievalQA
+from langchain.document_loaders import TextLoader
 from langchain.document_loaders import DirectoryLoader
 # import magic
 # import os
@@ -15,8 +16,9 @@ print("OPENAI_API_KEY found")
 openai_api_key=get_api_key()
 llm = OpenAI(temperature=0, openai_api_key=openai_api_key)
 
+
 # Get your loader ready
-loader = DirectoryLoader('data/', glob='**/*.txt')
+loader = DirectoryLoader('data/', glob='**/*.txt', loader_cls=TextLoader)
 # Load up your text into documents
 documents = loader.load()
 # Get your text splitter ready
